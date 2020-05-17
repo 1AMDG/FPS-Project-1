8ef95f58dd145d2577171b05959a01d40ce426e0
1AMDG <58343206+1AMDG@users.noreply.github.com>
2020-05-17 11:21:35 -0400

    FPS movement and camera control, menu UI system working (on and off menu)

---
+++ Assets/Scripts/UIControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour {
+    public GameObject escapeMenu;
+    // Start is called before the first frame update
+    void Awake () {
+        escapeMenu.SetActive (false);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown (KeyCode.Escape) && escapeMenu.activeInHierarchy == false) {
+            escapeMenu.SetActive (true);
+            print ("show");
+            Cursor.lockState = CursorLockMode.None;
+        } else if (Input.GetKeyDown (KeyCode.Escape) && escapeMenu.activeInHierarchy == true) {
+            escapeMenu.SetActive (false);
+            print ("deactive");
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    void NotShowing () {
+
+    }
+
+}
